1849b9fd4cb00f8a2a100997dcc0f3d7d584399a
meriem farsi <farsi.meriem60@gmail.com>
2020-08-04 18:28:15 +0000

    packing-list.js

--- Lighthouse-js-fundamentals/packing-list.js
+++ Lighthouse-js-fundamentals/packing-list.js
@@ -5,3 +5,10 @@ for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
 console.log(packingList[7]);
+
+let x=0
+while (x < packingList.length)
+{
+  console.log(packingList[x]);
+  x=x+1;
+}
